adfcbbafb074381475b45040a2b2e75b15cfb85d
SirDizzypanda <ardaardabatmaz@gmail.com>
2024-07-27 23:45:49 +0300

    First Commit

--- src/main/java/net/sirdizzypanda/xtrplugin/XtrPlugin.java
+++ src/main/java/net/sirdizzypanda/xtrplugin/XtrPlugin.java
@@ -1,17 +1,31 @@
 package net.sirdizzypanda.xtrplugin;
 
+import net.kyori.adventure.text.ComponentLike;
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Villager;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
-public final class XtrPlugin extends JavaPlugin {
+public final class XtrPlugin extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        System.out.println("Hello World");
 
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    @EventHandler
+    public void playerDeath(PlayerDeathEvent event) {
+        event.getPlayer().sendMessage("You died in " + event.getPlayer().getLocation().getBlockX()+" "+event.getPlayer().getLocation().getBlockY() + " " + event.getPlayer().getLocation().getBlockZ());
     }
+
 }
+
